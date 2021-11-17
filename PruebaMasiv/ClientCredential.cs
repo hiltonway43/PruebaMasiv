392ff82dab05bf2027f1b11dd5ec40ccf69cd303
Hilder Moscote Almanza <hilder.moscote@hotmail.com>
2021-11-17 16:30:09 -0500

    Agregar archivos de proyecto.

---
+++ PruebaMasiv/ClientCredential.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaMasiv
+{
+    public class ClientCredential
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+
+       public ClientCredential Validate(int Entity)
+        {
+
+            ClientCredential clientCredential = new ClientCredential();
+
+            switch (Entity)
+            {
+
+                case 1:
+                    clientCredential.User = "A1";
+                    clientCredential.Password = "B1";
+                    break;
+
+                case 2:
+                    clientCredential.User = "A2";
+                    clientCredential.Password = "B2";
+                    break;
+
+                default:
+                    clientCredential.User = string.Empty;
+                    clientCredential.Password = string.Empty;
+                    break;
+
+            }
+            return clientCredential;
+
+        }
+    }
+
+    
+}
---
+++ PruebaMasiv/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PruebaMasiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Digite el numero asociado al usuario:");     
+            int a = int.Parse(Console.ReadLine());
+            ClientCredential client = new ClientCredential();
+            Console.WriteLine("Usuario asignado {0} con Password {1}", client.Validate(a).User, client.Validate(a).Password);
+           
+        }
+
+      
+    }
+    
+
+}
